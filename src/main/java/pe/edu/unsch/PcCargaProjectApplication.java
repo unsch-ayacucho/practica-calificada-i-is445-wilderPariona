a2c66e9addf314de70b7256ba4cc19014c289b1c
WilderPariona <jhon.pariona.27@unsch.edu.pe>
2019-06-28 18:50:13 -0500

    creacion de la practica

---
+++ src/main/java/pe/edu/unsch/PcCargaProjectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcCargaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcCargaProjectApplication.class, args);
+	}
+
+}
